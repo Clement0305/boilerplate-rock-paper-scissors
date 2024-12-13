d63752910a65588ed4013d2c3eb1f0b9cc6be804
ElLamastos <mathis.onno@gmail.com>
2024-12-13 15:50:12 +0100

    v2

--- RPS.py
+++ RPS.py
@@ -8,14 +8,14 @@ def player(prev_play, opponent_history=[]):
     
     guess = random.choice(["R","S","P"])
     
-    if len(opponent_history) >= 7:
-        recent_moves = "".join(opponent_history[-7:])
+    if len(opponent_history) >= 9:
+        recent_moves = "".join(opponent_history[-9:])
         
         potential_moves = {'R': 0, 'P': 0, 'S': 0}
-        for i in range(len(opponent_history) - 7):
-            if "".join(opponent_history[i:i+7]) == recent_moves:
-                if i + 7 < len(opponent_history):  # Ensure index is in bounds
-                    next_move = opponent_history[i+7]
+        for i in range(len(opponent_history) - 9):
+            if "".join(opponent_history[i:i+9]) == recent_moves:
+                if i + 9 < len(opponent_history):  # Ensure index is in bounds
+                    next_move = opponent_history[i+9]
                     potential_moves[next_move] += 1
         
         predicted_move = max(potential_moves, key=potential_moves.get)
